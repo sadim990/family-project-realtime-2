3c1056455dfe58fa1350b47d1a6f3f7bc0f2a4bc
sadim990 <74527985+sadim990@users.noreply.github.com>
2021-07-29 17:33:50 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+
+function setup(){
+    createCanvas(400,400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,400,400);
+}
